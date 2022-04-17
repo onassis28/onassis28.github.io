3f30c27dd5b42d6a7a5ef2b989124e27830a6bed
Onassis <onassis.sowah@gmail.com>
2022-04-17 21:23:12 +0200

    built home, about and experience components

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import './experience.css'
+import {BsCheckCircle} from 'react-icons/bs'
+
+const Experience = () => {
+  return (
+    <section id='experience'>
+      <h5>The Skillset i bring along</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__skills">
+          <h3>Frontend Skills</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsCheckCircle/>
+              <div><h4>HTML5</h4>
+              <small>Experienced</small></div>
+              
+              </article>
+              <article className="experience__details">
+              <BsCheckCircle/>
+              <div><h4>CSS3</h4>
+              <small>Experienced</small></div>
+              
+              </article>
+              <article className="experience__details">
+              <BsCheckCircle/>
+              <div><h4>JavaScript</h4>
+              <small>Experienced</small></div>
+              
+              </article>
+              <article className="experience__details">
+              <BsCheckCircle/>
+              <div><h4>Tailwind CSS</h4>
+              <small>Experienced</small></div>
+              
+              </article>
+              <article className="experience__details">
+              <BsCheckCircle/>
+              <h4>React</h4>
+              <small>Experienced</small>
+              </article>
+              
+          
+          </div>
+         
+          
+          
+          </div>
+        
+          <div className="data__dev">
+          <h3>Data Skills</h3>
+            <div className="experience__content">
+            
+          <article className="experience__details">
+              <BsCheckCircle/>
+              <h4>Python</h4>
+              <small>Experienced</small>
+              </article>
+              <article className="experience__details">
+              <BsCheckCircle/>
+              <h4>Pandas/Numpy</h4>
+              <small>Experienced</small>
+              </article>
+              <article className="experience__details">
+              <BsCheckCircle/>
+              <h4>SQL/NoSQL</h4>
+              <small>Experienced</small>
+              </article>
+        
+              </div>
+            
+        </div>
+      
+      
+
+      </div>
+      
+      </section>
+  )
+}
+
+export default Experience
