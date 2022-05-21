616984a577e16f71148775a2bfdd5968ee385763
Onassis Sowah Anyetei <onassis.sowah@gmail.com>
2022-05-21 02:26:20 +0200

    implmenting routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import AboutPage from "./components/pages/Aboutpage";
 import './index.css'
 
 const root = ReactDOM.createRoot(
@@ -9,8 +10,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <BrowserRouter>
+  <Navbar/>
   <Routes>
     <Route path='/' element={<App />}/>
+    <Route path="./about" element={<Aboutpage/>}/>
     </Routes>
+    <Footer/>
   </BrowserRouter>
 );
