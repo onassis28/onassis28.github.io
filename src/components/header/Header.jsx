4a850a51ab855a11ed873bd9b8dcccd94b8aeedc
Onassis Sowah Anyetei <onassis.sowah@gmail.com>
2022-06-02 22:42:51 +0200

    ui changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,6 @@ import Cta from './Cta';
 import mage from '../../assets/mage.PNG';
 import Headersocial from './Headersocial';
 import { BsFillArrowDownCircleFill } from 'react-icons/bs';
-import { motion } from 'framer-motion';
 
 const Header = () => {
 	return (
@@ -12,12 +11,8 @@ const Header = () => {
 			<header className='header' id='home'>
 				<div className='container header__container'>
 					<h5 className='header__h5'>Hello I'm</h5>
-					<motion.h1
-						animate={{ rotate: 360 }}
-						transition={{ duration: 2 }}
-						className='header__h1'>
-						Onassis Sowah Anyetei
-					</motion.h1>
+
+					<h1>Onassis Sowah Anyetei</h1>
 					<h5 className='header__h5'>Software Engineer </h5>
 					<Cta />
 
